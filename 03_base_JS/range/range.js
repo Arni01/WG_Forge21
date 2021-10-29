118a03a0d10c05dc30fd635471a118e13644f2f7
Artsem Mukha <artsemmukha@gmail.com>
2021-10-29 17:57:30 +0300

    feat: add range

--- 03_base_JS/range/range.js
+++ 03_base_JS/range/range.js
@@ -1,39 +1,34 @@
-const getArray = (from = 0, to, step) => {
+const range = (from, to, step) => {
   let result = [];
-  step = step ? step : 1;
-  if (to > from) {
-    if (step < 1) return [];
-    for (let i = from; i < to; i += step) {
-      result.push(i);
-    }
-  } else {
-    for (let i = from; i > to; i -= Math.abs(step)) {
-      result.push(i);
-    }
+
+  if (from === to || (from < to && to < step)) return [from];
+  if (!step) {
+    step = from > to ? -1 : 1;
   }
+
+  if (to == null) {
+    if (from < 0) return [];
+    to = from || 0;
+    from = 0;
+  }
+  let length = Math.max(Math.ceil((to - from) / step), 0);
+  // if (!length && length !== 0) throw 'А для чего это ошибка ообще нужна?';
+  let test = Array(length);
+
+  for (let i = 0; i < length; i++) {
+    result.push(from);
+    from += step;
+  }
+
   return result;
 };
-const range = (...arg) => {
-  switch (arg.length) {
-    case 1:
-      if (arg[0] < 0) return [];
-      return getArray(undefined, arg[0]);
-    case 2:
-      if (arg[0] === arg[1]) return [arg[0]];
-      return getArray(arg[0], arg[1]);
-    case 3:
-      if(arg[0] < arg[1] && arg[1] < arg[2]) return [arg[0]]
-      return getArray(arg[0], arg[1], arg[2]);
-    default:
-      return [undefined];
-  }
-};
 
-// const range = (n, r, t) => {
+// const range2 = (n, r, t) => {
 //   if (n === r || (n < r && r < t)) return [n];
-//   //  if (n < r && r < t) return [n];
 //   if (r == null && n < 0) return [];
+//   // if (n < r && r < t) return [n];
 //   null == r && ((r = n || 0), (n = 0)), t || (t = r < n ? -1 : 1);
+
 //   for (
 //     var e = Math.max(Math.ceil((r - n) / t), 0), u = Array(e), i = 0;
 //     i < e;
@@ -43,15 +38,9 @@ const range = (...arg) => {
 //   }
 //   return u;
 // };
-// console.log(range(7, 8));
-// console.log(range(-5));
-// console.log(range2(-5));
 
-// console.log(range(10, -2, 2));
-// console.log(range(1, 2, 3));
-// console.log(range2(1, 2, 3));
-// console.log(range());
-// console.log(range(3, 1, 2));
-// console.log(range(-1, 11));
+// let a = [, ,];
+// console.log(range(...a));
+// console.log(range2(...a));
 
 module.exports = range;
