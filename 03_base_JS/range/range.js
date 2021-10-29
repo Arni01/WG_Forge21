eb7addeb7028d4abe6a2cf34377c8575ee05ccee
Artsem Mukha <artsemmukha@gmail.com>
2021-10-29 17:57:56 +0300

    fix: relise range

--- 03_base_JS/range/range.js
+++ 03_base_JS/range/range.js
@@ -23,24 +23,4 @@ const range = (from, to, step) => {
   return result;
 };
 
-// const range2 = (n, r, t) => {
-//   if (n === r || (n < r && r < t)) return [n];
-//   if (r == null && n < 0) return [];
-//   // if (n < r && r < t) return [n];
-//   null == r && ((r = n || 0), (n = 0)), t || (t = r < n ? -1 : 1);
-
-//   for (
-//     var e = Math.max(Math.ceil((r - n) / t), 0), u = Array(e), i = 0;
-//     i < e;
-//     i++, n += t
-//   ) {
-//     u[i] = n;
-//   }
-//   return u;
-// };
-
-// let a = [, ,];
-// console.log(range(...a));
-// console.log(range2(...a));
-
 module.exports = range;
